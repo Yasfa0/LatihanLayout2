d72b7e80c49bcd38af13fd814ba7bcb77833fc57
Muhammad Yasfa Zamzami <yasfazamzami0@Gmail.com>
2019-07-24 11:13:43 +0700

    Initial commit

---
+++ app/src/main/java/com/nas/latihanjava2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nas.latihanjava2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frame_layout2);
+    }
+}
